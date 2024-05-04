0f40c2aecf14c678b0bcba607e58bb0f0057d34d
Jerkie971 <121705951+Jerkie971@users.noreply.github.com>
2024-05-04 13:36:07 +0800

    ..

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -3,7 +3,7 @@
     <div>
       <div class="brand-logo d-flex align-items-center justify-content-between">
         <a href="./index.html" class="text-nowrap logo-img">
-          <img src="{{ asset('assets/images/logos/dark-logo.svg') }}" width="180" alt="" />
+          <!-- <h4>Helpdesk Management System</h4> -->
         </a>
         <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
           <i class="ti ti-x fs-8"></i>
